a25b1f7f2d5f9ad8f01e5f94c6a36a9f96565706
vincent-leguevel <vincent.leguevel.sio@gmail.com>
2017-07-02 23:54:05 +0200

    move packages

--- src/main/java/fr/bulb/App.java
+++ src/main/java/fr/bulb/App.java
@@ -1,9 +1,16 @@
 package fr.bulb;
 
-public class App 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+
     }
 }
