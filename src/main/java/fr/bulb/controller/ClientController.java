2310b3b275d5aa3301179b9c429842167e164bc0
vincent-leguevel <vincent.leguevel.sio@gmail.com>
2017-07-04 16:28:34 +0200

    Add drawing area canevas

--- src/main/java/fr/bulb/controller/ClientController.java
+++ src/main/java/fr/bulb/controller/ClientController.java
@@ -3,15 +3,30 @@ package fr.bulb.controller;
 import fr.bulb.view.Connection;
 import fr.bulb.view.Propos;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import javafx.stage.Stage;
 
 public class ClientController {
+
+    private Double lastXMouse = 0D;
+    private Double lastYMouse = 0D;
 
     @FXML
     javafx.scene.layout.BorderPane borderPane;
 
     @FXML
-    public void connection() throws Exception {
+    javafx.scene.control.Tab tabPassif;
+
+    @FXML
+    javafx.scene.control.ScrollBar sbPassif;
+
+    @FXML
+    javafx.scene.canvas.Canvas canevas;
+
+    @FXML
+    public void connection() {
         new Connection().createView();
     }
 
@@ -24,5 +39,14 @@ public class ClientController {
     public void quitter() throws Exception {
         Stage root = (Stage) borderPane.getScene().getWindow();
         root.close();
+    }
+
+    @FXML
+    public void click(MouseEvent e) {
+
+        GraphicsContext gc = canevas.getGraphicsContext2D();
+        gc.strokeLine(lastXMouse,lastYMouse,e.getX(),e.getY());
+        lastXMouse = e.getX();
+        lastYMouse = e.getY();
     }
 }
